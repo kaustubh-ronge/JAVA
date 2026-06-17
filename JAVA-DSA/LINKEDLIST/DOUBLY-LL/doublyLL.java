b74fb55a2414f5fcd0eb5bdb8f3bd43f686a6860
kaustubh-ronge <kaustubhronge25@gmail.com>
2026-06-17 09:21:41 +0530

    feat: implement Doubly Linked List with insertFirst and display methods

---
+++ JAVA-DSA/LINKEDLIST/DOUBLY-LL/doublyLL.java
@@ -0,0 +1,55 @@
+public class doublyLL {
+    Node head;
+
+    private class Node {
+        int val;
+        Node next;
+        Node prev;
+
+        public Node(int val) {
+            this.val = val;
+        }
+
+        public Node(int val, Node next, Node prev) {
+            this.val = val;
+            this.next = next;
+            this.prev = prev;
+        }
+
+    }
+
+    public void insertFirst(int val) {
+        Node node = new Node(val);
+
+        node.next = head;
+        node.prev = null;
+
+        if (head != null) {
+            head.prev = node;
+        }
+
+        head = node;
+    }
+
+    public void display() {
+        Node tempNode = head;
+        while (tempNode != null) {
+            System.out.print(tempNode.val + " -> ");
+            tempNode = tempNode.next;
+        }
+        System.out.println("END");
+    }
+
+    public static void main(String[] args) {
+        doublyLL dll = new doublyLL();
+
+        dll.insertFirst(1);
+        dll.insertFirst(2);
+        dll.insertFirst(3);
+        dll.insertFirst(4);
+        dll.insertFirst(5);
+
+        dll.display();
+    }
+
+}
